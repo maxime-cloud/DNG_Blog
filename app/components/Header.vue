e6334b5c981c607ef05bf58d0700525e7ff63d15
maxime-cloud <dongnemaxime@gmail.com>
2026-07-30 19:34:12 +0100

    update

--- app/components/Header.vue
+++ app/components/Header.vue
@@ -91,44 +91,45 @@
             />
           </template>
         </UPopover>
-        <UModal
-          scrollable
-          v-if="!store.isAuthenticated"
-          v-model:open="openRegister"
-          :ui="{
-            overlay: 'bg-CustomColor-900/60 backdrop-blur-xs',
-            content: 'ring-0 flex justify-center bg-transparent items-center'
-          }"
-        >
-          <button class="md:block hidden cursor-pointer">
-            S'inscrire
-          </button>
-          <template #content>
-            <RegisterForm @close-signup="openRegister = false" />
-          </template>
-        </UModal>
-        <UModal
-          scrollable
-          v-if="!store.isAuthenticated"
-          v-model:open="openConnexion"
-          :ui="{
-            overlay: 'bg-CustomColor-900/60 backdrop-blur-xs',
-            content: 'ring-0 flex justify-center bg-transparent items-center'
-          }"
-        >
-          <CUButton
-            class="md:flex hidden"
-            label="Connexion"
-            size="md"
-            logo-position="right"
-            logo-name="i-lucide-log-in"
-          />
-
-          <template #content>
-            <LoginForm @close-login="openConnexion = false" />
-          </template>
-        </UModal>
-
+        <template>
+          <UModal scrollable
+            v-if="!store.isAuthenticated"
+            v-model:open="openRegister"
+            :ui="{
+              overlay: 'bg-CustomColor-900/60 backdrop-blur-xs',
+              content: 'ring-0 flex justify-center bg-transparent items-center'
+            }"
+          >
+            <button class="md:block hidden cursor-pointer">
+              S'inscrire
+            </button>
+            <template #content>
+              <RegisterForm @close-signup="openRegister = false" />
+            </template>
+          </UModal>
+        </template>
+        <template>
+          <UModal scrollable
+            v-if="!store.isAuthenticated"
+            v-model:open="openConnexion"
+            :ui="{
+              overlay: 'bg-CustomColor-900/60 backdrop-blur-xs',
+              content: 'ring-0 flex justify-center bg-transparent items-center'
+            }"
+          >
+            <CUButton
+              class="md:flex hidden"
+              label="Connexion"
+              size="md"
+              logo-position="right"
+              logo-name="i-lucide-log-in"
+            />
+  
+            <template #content>
+              <LoginForm @close-login="openConnexion = false" />
+            </template>
+          </UModal>
+        </template>
         <UDrawer
           v-model:open="mobileMenuOpen"
           class="lg:hidden"
